99c17fb2bf457dd1d2457669f715354d28247ddf
AJ <arthur.james.thompson6@gmail.com>
2026-06-13 21:38:12 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,4 @@
+# Customer - stores their name, purchase history, and can do transactions
+# Food - stores its name, price, category, and popularity rating
+# Menu - stores the list of food items and can filter by category
+# Transaction - stores list of selected items and computes the total price
